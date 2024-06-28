ba7aaa7509fdf7cba6a54d359fbf25c074493c79
Hiromu <s1310089@u-aizu.ac.jp>
2024-06-28 12:39:29 +0900

    Final revision

--- HeadsOrTails.c
+++ HeadsOrTails.c
@@ -34,9 +34,9 @@ int main (){
     printf("Heads: %d, Tails: %d\n", totalHeads, totalTails);
 
     if (totalHeads > totalTails) {
-        printf("You won\n");
+        printf("%s won\n", name);
     } else {
-        printf("You lost\n");
+        printf("%s lost\n",name);
     }
     return 0;
 }
